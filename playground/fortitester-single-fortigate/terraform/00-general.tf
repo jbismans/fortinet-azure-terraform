c56243ee6f4d19c6bda251331d1f3a69f5873699
Joeri Van Hoof <jvanhoof@fortinet.com>
2020-03-30 15:52:13 +0200

    Upgrade syntax to latest terraform version and Azure provider

--- playground/fortitester-single-fortigate/terraform/00-general.tf
+++ playground/fortitester-single-fortigate/terraform/00-general.tf
@@ -13,9 +13,11 @@ variable "LOCATION" {
   description = "Azure region"
 }
 
-variable "USERNAME" {}
+variable "USERNAME" {
+}
 
-variable "PASSWORD" {}
+variable "PASSWORD" {
+}
 
 ##############################################################################################################
 # FortiGate license
@@ -46,49 +48,49 @@ provider "azurerm" {
 
 variable "vnet" {
   description = ""
-  default = "10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet" {
-  type        = "map"
+  type        = map(string)
   description = ""
 
   default = {
-    "1" = "10.0.0.0/24"         # port1
-    "2" = "10.0.1.0/24"         # port2
-    "3" = "10.0.2.0/24"         # mgmt
+    "1" = "10.0.0.0/24" # port1
+    "2" = "10.0.1.0/24" # port2
+    "3" = "10.0.2.0/24" # mgmt
   }
 }
 
 variable "subnetmask" {
-  type        = "map"
+  type        = map(string)
   description = ""
 
   default = {
-    "1" = "24"        # port1
-    "2" = "24"        # port2
-    "3" = "24"        # mgmt
+    "1" = "24" # port1
+    "2" = "24" # port2
+    "3" = "24" # mgmt
   }
 }
 
 variable "gateway_ipaddress" {
-  type        = "map"
+  type        = map(string)
   description = ""
 
   default = {
-    "1" = "10.0.0.1"      # port1
-    "2" = "10.0.1.1"      # port2
-    "3" = "10.0.2.1"      # mgmt
+    "1" = "10.0.0.1" # port1
+    "2" = "10.0.1.1" # port2
+    "3" = "10.0.2.1" # mgmt
   }
 }
 
 variable "fgt_ipaddress" {
-  type        = "map"
+  type        = map(string)
   description = ""
 
   default = {
-    "1" = "10.0.0.4"      # port1
-    "2" = "10.0.1.4"      # port2
+    "1" = "10.0.0.4" # port1
+    "2" = "10.0.1.4" # port2
   }
 }
 
@@ -101,13 +103,13 @@ variable "fgt_vmsize" {
 ##############################################################################################################
 
 variable "fts_ipaddress" {
-  type        = "map"
+  type        = map(string)
   description = ""
 
   default = {
-    "1" = "10.0.0.5"       # port1
-    "2" = "10.0.1.5"       # port2
-    "3" = "10.0.2.5"       # mgmt
+    "1" = "10.0.0.5" # port1
+    "2" = "10.0.1.5" # port2
+    "3" = "10.0.2.5" # mgmt
   }
 }
 
@@ -121,7 +123,7 @@ variable "fts_size" {
 
 resource "azurerm_resource_group" "resourcegroup" {
   name     = "${var.PREFIX}-RG"
-  location = "${var.LOCATION}"
+  location = var.LOCATION
 }
 
 ##############################################################################################################
--- playground/fortitester-single-fortigate/terraform/07-output.tf
+++ playground/fortitester-single-fortigate/terraform/07-output.tf
@@ -5,20 +5,21 @@
 ##############################################################################################################
 
 data "template_file" "summary" {
-  template = "${file("${path.module}/summary.tpl")}"
+  template = file("${path.module}/summary.tpl")
 
   vars = {
-    location = "${var.LOCATION}"
-    fgt_private_ip_address_port1 = "${azurerm_network_interface.fgt_ifc_port1.private_ip_address}"
-    fgt_private_ip_address_port2 = "${azurerm_network_interface.fgt_ifc_port2.private_ip_address}"
-    fgt_public_ip_address = "${data.azurerm_public_ip.fgt_pip.ip_address}"
-    fts_private_ip_address_port1 = "${azurerm_network_interface.fts_ifc_port1.private_ip_address}"
-    fts_private_ip_address_port2 = "${azurerm_network_interface.fts_ifc_port2.private_ip_address}"
-    fts_private_ip_address_mgmt = "${azurerm_network_interface.fts_ifc_mgmt.private_ip_address}"
-    fts_public_ip_address = "${data.azurerm_public_ip.fts_pip.ip_address}"
+    location                     = var.LOCATION
+    fgt_private_ip_address_port1 = azurerm_network_interface.fgt_ifc_port1.private_ip_address
+    fgt_private_ip_address_port2 = azurerm_network_interface.fgt_ifc_port2.private_ip_address
+    fgt_public_ip_address        = data.azurerm_public_ip.fgt_pip.ip_address
+    fts_private_ip_address_port1 = azurerm_network_interface.fts_ifc_port1.private_ip_address
+    fts_private_ip_address_port2 = azurerm_network_interface.fts_ifc_port2.private_ip_address
+    fts_private_ip_address_mgmt  = azurerm_network_interface.fts_ifc_mgmt.private_ip_address
+    fts_public_ip_address        = data.azurerm_public_ip.fts_pip.ip_address
   }
 }
 
 output "deployment_summary" {
-  value = "${data.template_file.summary.rendered}"
+  value = data.template_file.summary.rendered
 }
+
